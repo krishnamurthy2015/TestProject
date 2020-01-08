b17a5bbf537cfe58fc3624cbb2d669ecd04236a3
Krishnamurthy.D <Krishnamurthy.D@BLRKEC129101L.ad.infosys.com>
2020-01-08 14:53:57 +0530

    Changes in .class file

--- FirstJenkinsProject/src/main/java/com/example/test/FirstJenkinsProject/HelloWorld.java
+++ FirstJenkinsProject/src/main/java/com/example/test/FirstJenkinsProject/HelloWorld.java
@@ -9,5 +9,9 @@ public class HelloWorld
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        int[] data= {0,3,7,9,10,13,15};
+        
+        System.out.println(data[data[1]]);
     }
 }
